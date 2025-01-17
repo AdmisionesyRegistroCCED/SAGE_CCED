72a84886b97dfc22bd2582a27821439d6cedb991
ccedmanager <communitymanagercced@gmail.com>
2025-01-17 08:09:42 -0500

    cambios en formulario ACA 003

--- public/student_register.php
+++ public/student_register.php
@@ -34,71 +34,87 @@ if (substr($binperms, 9, 1) == 2) {
                         <a class="auploadcsv" href="../src/controllers/uploadcsv.php">Cargar Csv</a>
                     </div>
                     <hr><br>
-                    <form id="formRegistrarEstudiante">
-                        <div class="container_register">
-                            <div>
-                                <label for="estudiantes_nombre">Nombres *</label>
-                                <input type="text" id="estudiantes_nombre" name="fname" required>
-                            </div>
-                            <div>
-                                <label for="lname">Apellidos *</label>
-                                <input type="text" id="estudiantes_apellidos" name="lname" required>
-                            </div>
-                            <div>
-                                <label for="estudiantes_tipo_documento">Tipo de documento *</label>
-                                <select name="dni_type" id="estudiantes_tipo_documento" required>
-                                    <option value="--">Seleccione una opción</option>
-                                    <option value="ti">Tarjeta de identidad</option>
-                                    <option value="cc">Cedula de ciudadania</option>
-                                    <option value="ce">Cedula de extranjeria</option>
-                                </select>
-                            </div>
-                            <div>
-                                <label for="estudiantes_no_documento">Numero de documento *</label>
-                                <input type="number" id="estudiantes_no_documento" name="dni" required>
-                            </div>
-                            <div>
-                                <label for="estudiantes_fecha_nacimiento">Fecha de nacimiento</label>
-                                <input type="date" id="estudiantes_fecha_nacimiento" name="fecha_nacimiento" required>
-                            </div>
-                            <div>
-                                <label for="estudiantes_genero">Genero</label>
-                                <select id="estudiantes_genero" name="gender" required>
-                                    <option value="Nulo">Seleccione una opción</option>
-                                    <option value="Masculino">Masculino</option>
-                                    <option value="Femenino">Femenino</option>
-                                </select>
-                            </div>
-                            <div>
-                                <label for="estudiantes_telefono">Teléfono (Fijo/Celular)</label>
-                                <input type="text" id="estudiantes_telefono" name="phone" required>
-                            </div>
-                            <div>
-                                <label for="estudiantes_direccion">Dirección</label>
-                                <input type="text" id="estudiantes_direccion" required>
-                            </div>
-                            <div>
-                                <label for="estudiantes_correo">Correo electronico *</label>
-                                <input type="email" id="estudiantes_correo" name="email" required>
-                            </div>
-                            <div>
-                                <label for="estudiantes_estado">Estado</label>
-                                <select name="status" id="estudiantes_estado" required>
-                                    <option value="activo">Activo</option>
-                                    <option value="inactivo">Inactivo</option>
-                                    <option value="Egresado">Egresado</option>
-                                </select>
-                            </div>
+                    <form id="formRegistrarEstudiante" class="container_register">
+
+                        <div>
+                            <label for="estudiantes_nombre">Nombres *</label>
+                            <input type="text" id="estudiantes_nombre" name="fname" required>
+                        </div>
+                        <div>
+                            <label for="lname">Apellidos *</label>
+                            <input type="text" id="estudiantes_apellidos" name="lname" required>
+                        </div>
+                        <div>
+                            <label for="estudiantes_tipo_documento">Tipo de documento *</label>
+                            <select name="dni_type" id="estudiantes_tipo_documento" required>
+                                <option value="--">Seleccione una opción</option>
+                                <option value="ti">Tarjeta de identidad</option>
+                                <option value="cc">Cedula de ciudadania</option>
+                                <option value="ce">Cedula de extranjeria</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label for="estudiantes_no_documento">Numero de documento *</label>
+                            <input type="number" id="estudiantes_no_documento" name="dni" required>
+                        </div>
+
+
+                        <div id="fecha">
+
+                            <label for="estudiantes_fecha_nacimiento">Fecha de nacimiento</label>
+                            <input type="date" id="estudiantes_fecha_nacimiento" name="fecha_nacimiento" required>
+                        </div>
+
+
+                        <div id="genero">
+                            <label for="estudiantes_genero">Genero</label>
+                            <select id="estudiantes_genero" name="gender" required>
+                                <option value="Nulo">Seleccione una opción</option>
+                                <option value="Masculino">Masculino</option>
+                                <option value="Femenino">Femenino</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label for="estudiantes_depto_exp">Departamento</label>
+                            <input type="text" id="estudiantes_depto_exp" name="fecha_nacimiento" required>
+                        </div>
+                        <div>
+                            <label for="estudiantes_ciudad_exp">Ciudad de expedición</label>
+                            <input type="text" id="estudiantes_ciudad_exp" name="fecha_nacimiento" required>
+                        </div>
+
+
+                        <div>
+
+                            <label for="estudiantes_direccion">Teléfono (Fijo/Celular)</label>
+                            <input type="text" id="estudiantes" name="address" required>
+                        </div>
+                        <div>
+                            <label for="estudiantes_direccion">Dirección</label>
+                            <input type="text" id="estudiantes_direccion" required>
+                        </div>
+                        <div>
+                            <label for="estudiantes_correo">Correo electronico *</label>
+                            <input type="email" id="estudiantes_correo" name="email" required>
+                        </div>
+                        <div>
+                            <label for="estudiantes_estado">Estado</label>
+                            <select name="status" id="estudiantes_estado" required>
+                                <option value="activo">Activo</option>
+                                <option value="inactivo">Inactivo</option>
+                                <option value="Egresado">Egresado</option>
+                            </select>
                         </div>
                         <div class="full-witdh ">
                             <div class="container_register no-Flex">
                                 <label for="estudiantes_observaciones">Observaciones:</label>
                                 <textarea name="" id="estudiantes_observaciones" rows="5" cols="50" resize=""></textarea>
                             </div>
-                            <div class="container_register">
-                                <button type="submit" class="submit-btn" id="btnRegistrar">Registrar</button>
-                            </div>
                         </div>
+                        <div class="">
+                            <button type="submit" class="submit-btn" id="btnRegistrar">Registrar</button>
+                        </div>
+
                     </form>
                 </div>
                 <footer class=".footer">
